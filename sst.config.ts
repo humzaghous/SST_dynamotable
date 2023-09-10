356e7167c39fbe668071f6b9cac6e03c29f35dbd
Hamza Ghous <91271450+hamzaghous@users.noreply.github.com>
2023-09-10 10:41:58 +0500

    First commit

---
+++ sst.config.ts
@@ -0,0 +1,13 @@
+import { SSTConfig } from "sst";
+import { StorageStack } from "./stacks/StorageStack";
+export default {
+config(_input) {
+return {
+name: "notes",
+region: "us-east-1",
+};
+},
+stacks(app) {
+app.stack(StorageStack);
+},
+} satisfies SSTConfig;
